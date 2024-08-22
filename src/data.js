b3d98a4a4409332945055792b959657a3e657ade
Praneshxo <praneshgara@gmail.com>
2024-08-22 09:19:19 +0530

    insta

---
+++ src/data.js
@@ -0,0 +1,14 @@
+const dataArray = [
+  {
+    image: 'https://example.com/image1.jpg',
+    paragraph: 'Paragraph for image 1.',
+    heading: 'Heading 1'
+  },
+  {
+    image: 'https://example.com/image2.jpg',
+    paragraph: 'Paragraph for image 2.',
+    heading: 'Heading 2'
+  },
+  // Add more data objects as needed
+];
+ export default dataArray;
